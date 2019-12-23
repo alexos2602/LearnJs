6d7c5d13117354c417af24310e334d0771aa0414
jose <alejose-12@hotmail.com>
2019-12-23 01:01:31 -0400

    variables y tipos de datos

---
+++ notas.js
@@ -0,0 +1,47 @@
+/*//variables
+
+    let precio = 100,
+    impuesto = precio * 0.28,
+    descuent = precio * 0.1;
+
+    let preFinal = precio + impuesto - descuent;
+
+    console.log(precio);
+    console.log(impuesto);
+    console.log(descuent);
+    console.log(preFinal);
+
+    const echoEn = 1996;
+
+    //let tienen ambito de bloque fuera del bloque no se toma en cuenta
+    //var no es asi 
+    */
+
+
+//tipos de datos
+
+    //primitivos
+
+    //string texto
+    //number 
+    //Boolean verdadero o falso
+    //undefined
+    //null
+
+    let hola = "hola \"todos\"";
+
+    //template Strings para concatenar amiguito
+
+    let nombPerso = "Jose"; 
+    let num = 4;
+    let saludo = console.log(` Bienvenido, ${nombPerso}, ${num * 2} veces`);//backtick alt+96
+
+    
+
+    //Compuestos u objetos
+
+    //Array
+    //Object
+    //Map
+    //Set
+    //function
