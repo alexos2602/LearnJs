8d12d7b5c72e7244523a62df1ab23a89e3b58334
jose <alejose-12@hotmail.com>
2020-01-04 01:21:51 -0400

    Array, For

--- notas.js
+++ notas.js
@@ -299,3 +299,38 @@
         console.log(valor);
     } */
 
+/* ------------------------------------------------------------------------ */
+
+    //Array
+
+    let arr = [1, '2', 3, 4, 'true', 5, 6];
+             //0  1  2  3  4
+
+    /* console.log(arr.length);
+    console.log(arr[0]); */
+
+    /* for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+        console.log(element)
+    } */
+
+    /* let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+        if (typeof element == 'number') {
+            sum += element;
+        }
+    }
+
+    console.log(sum); */
+
+    /* let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+        if (typeof element != 'number') continue;
+            sum += element;
+    }
+
+    console.log(sum);
+    */
+
