054ba470e62ff071425e1488a3b375c675cf8ac7
José <alejose-12@hotmail.com>
2020-02-27 16:42:31 -0400

    Ejercicios 1-7

---
+++ ejercicios/main2.js
@@ -0,0 +1,8 @@
+function sumaNumeros() {
+    let num1 = 5;
+    let num2 = 9;
+
+    document.write(" Crea una función que se cargue al inicio y que muestre la suma de dos números ");
+    document.write(num1 + num2);
+}
+
---
+++ ejercicios/main4.js
@@ -0,0 +1,10 @@
+function muestraOpcion() {
+    var opciones = document.getElementsByName("eleccion");
+    console.log(opciones);
+    for (let i = 0; i < opciones.length; i++) {
+
+        if (opciones[i].checked) {
+            alert(opciones[i].value)
+        }
+    }
+}
