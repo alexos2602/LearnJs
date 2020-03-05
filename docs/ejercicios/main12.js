a1a393226df1755ed56d518861936aa34a3417d0
José <alejose-12@hotmail.com>
2020-03-04 20:45:30 -0400

    ordenando un array

---
+++ docs/ejercicios/main12.js
@@ -0,0 +1,43 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function ordenar() {
+      
+    let array = [];
+
+    for (let i = 0; i < 10; i++) {
+        array.push(getRandomInt(1, 100));
+        
+    }
+
+    console.log("Array sin ordenar");
+    
+    console.log(array);
+
+    array.sort(function (a, b) { return a - b});
+    
+    console.log("Array con ordenar");
+
+    console.log(array)
+
+    let ul = document.createElement("ul");
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+        let li = document.createElement("li");
+
+        let textLi = document.createTextNode(element);
+
+        li.appendChild(textLi);
+
+        ul.appendChild(li);
+    }
+
+    let valores = document.getElementById("valores");
+
+    valores.appendChild(ul);
+  }
+
+  window.onload = ordenar;
